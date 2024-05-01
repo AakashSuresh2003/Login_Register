2281515c4bd45b03c2b35a45c042c47c9ee04f90
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-05-01 21:32:17 +0530

    Feat : Added user schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,26 @@
-// Here, the schema for the required model will be implemented
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username must be provided"],
+      minLength: [6, "Username must be above 6 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email must be provided"],
+      unique: [true, "e-mail must be unique"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password must be provided"],
+      minLength: [6, "Password must be above 6 characters"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
